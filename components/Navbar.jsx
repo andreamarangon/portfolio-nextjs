bcf260a0feee5fef9b512361148715c24cd747bd
andreamarangon <marangonandrea84@gmail.com>
2023-10-06 11:06:25 +0200

    chore: install react-icons, font, Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,139 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from 'react-icons/ai'
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [shadow, setShadow] = useState(false);
+  const [navBg, setNavBg] = useState('#ecf0f3');
+  const [linkColor, setLinkColor] = useState('#1f2937');
+
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setShadow(true);
+      } else {
+        setShadow(false);
+      }
+    };
+    window.addEventListener('scroll', handleShadow);
+  }, []);
+
+
   return (
-    <div>Navbar</div>
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <Image src="/../assets/am-logo.png" alt="/" width="60" height="60" priority />
+        <div>
+          <ul className='hidden md:flex'>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Info</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Progetti</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Contatti</li>
+            </Link>
+          </ul>
+          <div onClick={handleNav} className='md:hidden'>
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+      {/* Mobile Menu */}
+      {/* Overlay */}
+      <div className={nav && 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70'}>
+        <div className={
+          nav
+            ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+            : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+        }>
+          <div>
+            <div className='flex w-full items-center justify-between'>
+              <Image src='/../assets/am-logo.png' width='45' height='45' alt='/' />
+              <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className='border-b border-gray-300 my-4'>
+              <p className='w-[85%] md:w-[90%] py-4'>Let&#39;s build something legendary together</p>
+            </div>
+          </div>
+          <div className='py-4 flex flex-col'>
+            <ul className='uppercase'>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Home</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Info</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Skills</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Progetti</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Contatti</li>
+              </Link>
+            </ul>
+            <div className='pt-40'>
+              <p className='uppercase tracking-widest text-[#9D1D38]'>Let's Connect</p>
+              <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                <a
+                  href='https://www.linkedin.com/in/marangon-andrea'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaLinkedinIn />
+                  </div>
+                </a>
+                <a
+                  href='https://github.com/andreamarangon'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <FaGithub />
+                  </div>
+                </a>
+                <Link href='/#contact'>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                    <AiOutlineMail />
+                  </div>
+                </Link>
+                <Link href='/resume'>
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'
+                  >
+                    <BsFillPersonLinesFill />
+                  </div>
+                </Link>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
