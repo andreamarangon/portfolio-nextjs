3d0ff5a278b930e8303038c52e53227ead9d6bcf
andreamarangon <marangonandrea84@gmail.com>
2025-05-07 17:04:22 +0200

    refactor: implement mobile hover effect for ProjectItem component

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 const ProjectItem = ({ title, backgroundImg, tech, projectUrl }) => {
   return (
-    <div className='relative p-4 flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 dark:shadow-gray-900 rounded-xl group hover:bg-linear-to-r from-[#9D1D38] to-[#5651e5]'>
-      <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' priority />
-      <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+    <div className='relative p-4 flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 dark:shadow-gray-900 rounded-xl group mobile-hover:bg-linear-to-r from-[#9D1D38] to-[#5651e5]'>
+      <Image className='rounded-xl group-mobile-hover:opacity-10' src={backgroundImg} alt='/' priority />
+      <div className='hidden group-mobile-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
         <p className='pb-4 pt-2 text-white text-center'>{tech}</p>
         <Link href={projectUrl}>
